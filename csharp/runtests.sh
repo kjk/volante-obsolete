ea0fc185c63e555be3837eb9bb4118b2795d2fa1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-08 15:05:20 -0700

    add runtests.sh (unix test runner)

---
+++ csharp/runtests.sh
@@ -0,0 +1,34 @@
+# Runs all tests
+# First argument is the name of bin directory where the executables are
+
+rm *.dbs
+mono ${1}/TestIndex.exe
+rm *.dbs
+mono ${1}/TestIndex.exe altbtree
+rm *.dbs
+mono ${1}/TestIndex.exe altbtree serializable
+rm *.dbs
+mono ${1}/TestIndex.exe inmemory
+mono ${1}/TestIndex2.exe
+mono ${1}/TestEnumerator.exe
+rm *.dbs
+mono ${1}/TestEnumerator.exe altbtree
+mono ${1}/TestCompoundIndex.exe
+mono ${1}/TestRtree.exe
+mono ${1}/TestR2.exe
+mono ${1}/TestTtree.exe
+mono ${1}/TestRaw.exe
+mono ${1}/TestRaw.exe
+mono ${1}/TestGC.exe
+mono ${1}/TestGC.exe background
+mono ${1}/TestGC.exe background altbtree
+mono ${1}/TestConcur.exe
+mono ${1}/TestXML.exe
+mono ${1}/TestBackup.exe
+mono ${1}/TestBlob.exe
+mono ${1}/TestBlob.exe
+mono ${1}/TestTimeSeries.exe
+mono ${1}/TestBit.exe
+mono ${1}/TestList.exe
+mono ${1}/TestReplic.exe master&
+mono ${1}/TestReplic.exe slave
