39dddc6e9f0e3a419c21a713db06ed9dc0dcca2c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-13 15:19:42 +0200

    remove use of USE_GENERICS in BitIndex.cs

--- csharp/src/BitIndex.cs
+++ csharp/src/BitIndex.cs
@@ -1,11 +1,8 @@
 namespace NachoDB
 {
     using System;
-#if USE_GENERICS
     using System.Collections.Generic;
-#else
     using System.Collections;
-#endif
 
     /// <summary>
     /// Interface of bit index.
@@ -15,11 +12,7 @@ namespace NachoDB
     /// some binary or boolean property, for example "sex", but it is possible to 
     /// use group of bits to represent enumerations with more possible values.
     /// </summary>
-#if USE_GENERICS
     public interface BitIndex<T> : IPersistent, IResource, ICollection<T> where T:class,IPersistent
-#else
-    public interface BitIndex : IPersistent, IResource, ICollection 
-#endif
     { 
         /// <summary>
         /// Get properties of specified object
@@ -28,11 +21,7 @@ namespace NachoDB
         /// <returns>bit mask associated with this objects</returns>
         /// <exception cref="NachoDB.StorageError">StorageError(StorageError.ErrorCode.KEY_NOT_FOUND) exception if there is no such object in the index
         /// </exception>
-#if USE_GENERICS
         int Get(T obj);
-#else 
-        int Get(IPersistent obj);
-#endif
 
         /// <summary>
         /// Put new object in the index. If such objct already exists in index, then its
@@ -42,34 +31,15 @@ namespace NachoDB
         /// its forced to become persistent by assigning OID to it.
         /// </param>
         /// <param name="mask">bit mask associated with this objects</param>
-#if USE_GENERICS
         void Put(T obj, int mask);
-#else
-        void Put(IPersistent obj, int mask);
-#endif
-
 
         /// <summary> Access object bitmask
         /// </summary>
-#if USE_GENERICS
         int this[T obj] 
-#else
-        int this[IPersistent obj] 
-#endif
         {
             get;
             set;
         }       
-
-#if !USE_GENERICS
-        /// <summary>
-        /// Remove object from the index 
-        /// </summary>
-        /// <param name="obj">object removed from the index
-        /// </param>
-        /// <returns><code>true</code> if member was successfully removed or <code>false</code> if member is not found</returns>
-        bool Remove(IPersistent obj);
-#endif
 
         /// <summary> Get number of objects in the index
         /// </summary>
@@ -78,23 +48,13 @@ namespace NachoDB
         /// </returns>
         int Size();
 
-#if !USE_GENERICS
-        /// <summary> Remove all objects from the index
-        /// </summary>
-        void Clear();
-#endif
-
         /// <summary>
         /// Get enumerator for selecting objects with specified properties.
         /// </summary>
         /// <param name="setBits">bitmask specifying bits which should be set (1)</param>
         /// <param name="clearBits">bitmask specifying bits which should be cleared (0)</param>
         /// <returns>enumerator</returns>
-#if USE_GENERICS
         IEnumerator<T> GetEnumerator(int setBits, int clearBits);
-#else
-        IEnumerator GetEnumerator(int setBits, int clearBits);
-#endif
 
         /// <summary>
         /// Get enumerable collection for selecting objects with specified properties.
@@ -102,11 +62,7 @@ namespace NachoDB
         /// <param name="setBits">bitmask specifying bits which should be set (1)</param>
         /// <param name="clearBits">bitmask specifying bits which should be cleared (0)</param>
         /// <returns>enumerable collection</returns>
-#if USE_GENERICS
         IEnumerable<T> Select(int setBits, int clearBits);
-#else 
-        IEnumerable Select(int setBits, int clearBits);
-#endif
     }
 }
 
