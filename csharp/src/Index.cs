d082eddb189a7992f91fbcb773dedc5313e533f9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-13 19:36:16 +0200

    remove use of USE_GENERICS in Index.cs

--- csharp/src/Index.cs
+++ csharp/src/Index.cs
@@ -1,12 +1,9 @@
 namespace NachoDB
 {
     using System;
-#if USE_GENERICS
-    using System.Collections.Generic;
-#else
     using System.Collections;
-#endif
-	    
+    using System.Collections.Generic;
+
     /// <summary> Interface of object index.
     /// Index is used to provide fast access to the object by key. 
     /// Object in the index are stored ordered by key value. 
@@ -15,11 +12,7 @@ namespace NachoDB
     /// (each boundary can be specified or unspecified and can be inclusive or exclusive)
     /// Key should be of scalar, String, java.util.Date or peristent object type.
     /// </summary>
-#if USE_GENERICS
     public interface Index<K,V> : GenericIndex<K,V> where V:class,IPersistent
-#else
-    public interface Index : GenericIndex
-#endif
     {    
         /// <summary> Put new object in the index. 
         /// </summary>
@@ -32,11 +25,7 @@ namespace NachoDB
         /// <code>false</code> if index was declared as unique and there is already object with such value
         /// of the key in the index. 
         /// </returns>
-#if USE_GENERICS
         bool Put(Key key, V obj);
-#else
-        bool Put(Key key, IPersistent obj);
-#endif
 
         /// <summary> Put new object in the index. 
         /// </summary>
@@ -50,11 +39,7 @@ namespace NachoDB
         /// of the key in the index. 
         /// 
         /// </returns>
-#if USE_GENERICS
         bool Put(K key, V obj);
-#else
-        bool Put(object key, IPersistent obj);
-#endif
 
         /// <summary> Associate new value with the key. If there is already object with such key in the index, 
         /// then it will be removed from the index and new value associated with this key.
@@ -66,11 +51,7 @@ namespace NachoDB
         /// </param>
         /// <returns>object previously associated with this key, <code>null</code> if there was no such object
         /// </returns>
-#if USE_GENERICS
         V Set(Key key, V obj);
-#else
-        IPersistent Set(Key key, IPersistent obj);
-#endif
 
         /// <summary> Associate new value with the key. If there is already object with such key in the index, 
         /// then it will be removed from the index and new value associated with this key.
@@ -82,11 +63,7 @@ namespace NachoDB
         /// </param>
         /// <returns>object previously associated with this key, <code>null</code> if there was no such object
         /// </returns>
-#if USE_GENERICS
         V Set(K key, V obj);
-#else
-        IPersistent Set(object key, IPersistent obj);
-#endif
 
         /// <summary> Remove object with specified key from the tree.
         /// </summary>
@@ -97,11 +74,7 @@ namespace NachoDB
         /// <exception cref="NachoDB.StorageError">StorageError(StorageError.ErrorCode.KEY_NOT_FOUND) exception if there is no such key in the index
         /// 
         /// </exception>
-#if USE_GENERICS
         void  Remove(Key key, V obj);
-#else
-        void  Remove(Key key, IPersistent obj);
-#endif
 
         /// <summary> Remove object with specified key from the tree.
         /// </summary>
@@ -112,11 +85,7 @@ namespace NachoDB
         /// <exception cref="NachoDB.StorageError">StorageError(StorageError.ErrorCode.KEY_NOT_FOUND) exception if there is no such key in the index
         /// 
         /// </exception>
-#if USE_GENERICS
         void  Remove(K key, V obj);
-#else
-        void  Remove(object key, IPersistent obj);
-#endif
 
         /// <summary> Remove key from the unique index.
         /// </summary>
@@ -127,11 +96,7 @@ namespace NachoDB
         /// or StorageError(StorageError.ErrorCode.KEY_NOT_UNIQUE) if index is not unique.
         /// 
         /// </exception>
-#if USE_GENERICS
         V  Remove(Key key);
-#else
-        IPersistent  Remove(Key key);
-#endif
 
         /// <summary> Remove key from the unique index.
         /// </summary>
@@ -142,10 +107,6 @@ namespace NachoDB
         /// or StorageError(StorageError.ErrorCode.KEY_NOV_UNIQUE) if index is not unique.
         /// 
         /// </exception>
-#if USE_GENERICS
         V RemoveKey(K key);
-#else
-        IPersistent  Remove(object key);
-#endif
     }
 }
