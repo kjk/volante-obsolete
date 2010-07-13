93510c9537eab2767ada00112bd3538988a2dd65
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-13 19:41:00 +0200

    remove use of USE_GENERICS in DefaultPersistentComparator.cs

--- csharp/src/impl/DefaultPersistentComparator.cs
+++ csharp/src/impl/DefaultPersistentComparator.cs
@@ -3,7 +3,6 @@ namespace NachoDB.Impl
     using System;
     using NachoDB;
 
-#if USE_GENERICS
     public class DefaultPersistentComparator<K,V> : PersistentComparator<K,V> where V:class,IPersistent,IComparable<V>,IComparable<K> 
     { 
         public override int CompareMembers(V m1, V m2) {
@@ -14,15 +13,4 @@ namespace NachoDB.Impl
             return mbr.CompareTo(key);
         }
     }
-#else
-    public class DefaultPersistentComparator : PersistentComparator { 
-        public override int CompareMembers(IPersistent m1, IPersistent m2) {
-            return ((IComparable)m1).CompareTo(m2);
-        }
-        
-        public override int CompareMemberWithKey(IPersistent mbr, object key) { 
-            return ((IComparable)mbr).CompareTo(key);
-        }
-    }
-#endif
 }
