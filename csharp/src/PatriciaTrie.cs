594771c3e00e362c10b43ac9b118260e78142cda
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-13 19:39:53 +0200

    remove use of USE_GENERICS in PatriciaTree.cs

--- csharp/src/PatriciaTrie.cs
+++ csharp/src/PatriciaTrie.cs
@@ -1,9 +1,6 @@
 using System;
-#if USE_GENERICS
-using System.Collections.Generic;
-#else
 using System.Collections;
-#endif
+using System.Collections.Generic;
 
 namespace NachoDB
 {
@@ -17,11 +14,7 @@ namespace NachoDB
     /// This structure has it's main use in IP routing software, but can provide an interesting alternative 
     /// to other structures such as hashtables when memory space is of concern.
     /// </summary>
-#if USE_GENERICS
     public interface PatriciaTrie<T> : IPersistent, IResource, ICollection<T> where T:class,IPersistent 
-#else
-    public interface PatriciaTrie : IPersistent, IResource, ICollection 
-#endif
     { 
         /// <summary> 
         /// Add new key to the trie
@@ -30,50 +23,27 @@ namespace NachoDB
         /// <param name="obj">persistent object associated with this key</param>
         /// <returns>previous object associtated with this key or <code>null</code> if there
         /// was no such object</returns>
-#if USE_GENERICS
         T Add(PatriciaTrieKey key, T obj);
-#else
-        IPersistent Add(PatriciaTrieKey key, IPersistent obj);
-#endif
     
         /// <summary>
         /// Find best match with specified key
         /// </summary>
         /// <param name="key">bit vector</param>
         /// <returns>object associated with this deepest possible match with specified key</returns>
-#if USE_GENERICS
         T FindBestMatch(PatriciaTrieKey key);
-#else
-        IPersistent FindBestMatch(PatriciaTrieKey key);
-#endif
     
         /// <summary>
         /// Find exact match with specified key
         /// </summary>
         /// <param name="key">bit vector</param>
         /// <returns>object associated with this key or NULL if match is not found</returns>
-#if USE_GENERICS
         T FindExactMatch(PatriciaTrieKey key);
-#else
-        IPersistent FindExactMatch(PatriciaTrieKey key);
-#endif
     
         /// <summary>
         /// Removes key from the triesKFind exact match with specified key
         /// </summary>
         /// <param name="key">bit vector</param>
         /// <returns>object associated with removed key or <code>null</code> if such key is not found</returns>
-#if USE_GENERICS
         T Remove(PatriciaTrieKey key);
-#else
-        IPersistent Remove(PatriciaTrieKey key);
-#endif
-
-#if !USE_GENERICS
-        /// <summary>
-        /// Clear the trie: remove all elements from trie
-        /// </summary>
-        void Clear();
-#endif
     }
 }
