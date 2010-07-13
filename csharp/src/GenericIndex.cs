38a16a141363bdb6490c981f51ea66451c9796b1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-13 15:26:58 +0200

    remove use of USE_GENERICS in GenericIndex.cs

--- csharp/src/GenericIndex.cs
+++ csharp/src/GenericIndex.cs
@@ -1,10 +1,8 @@
 namespace NachoDB
 {
     using System;
-#if USE_GENERICS
-    using System.Collections.Generic;
-#endif
     using System.Collections;
+    using System.Collections.Generic;
 
     public enum IterationOrder 
     {
@@ -20,33 +18,21 @@ namespace NachoDB
     /// (each boundary can be specified or unspecified and can be inclusive or exclusive)
     /// Key should be of scalar, String, java.util.Date or peristent object type.
     /// </summary>
-#if USE_GENERICS
     public interface GenericIndex {}
 
     public interface GenericIndex<K,V> : IPersistent, IResource, ICollection<V>, GenericIndex where V:class,IPersistent
-#else
-    public interface GenericIndex : IPersistent, IResource, ICollection
-#endif
     {
         /// <summary> Access element by key
         /// </summary>
-#if USE_GENERICS
         V this[K key] 
-#else
-        IPersistent this[object key] 
-#endif
-        {
+    {
             get;
             set;
         }       
 
         /// <summary> Get objects which key value belongs to the specified range.
         /// </summary>
-#if USE_GENERICS
         V[] this[K from, K till] 
-#else
-        IPersistent[] this[object from, object till] 
-#endif
         {
             get;
         }       
@@ -61,11 +47,7 @@ namespace NachoDB
         /// one objects in the index with specified value of the key.
         /// 
         /// </exception>
-#if USE_GENERICS
         V Get(Key key);
-#else
-        IPersistent Get(Key key);
-#endif
 
         /// <summary> Get object by key (exact match)     
         /// </summary>
@@ -77,11 +59,7 @@ namespace NachoDB
         /// one objects in the index with specified value of the key.
         /// 
         /// </exception>
-#if USE_GENERICS
         V Get(K key);
-#else
-        IPersistent Get(object key);
-#endif
 
         /// <summary> Get objects which key value belongs to the specified range.
         /// Either from boundary, either till boundary either both of them can be <code>null</code>.
@@ -96,11 +74,7 @@ namespace NachoDB
         /// <returns>array of objects which keys belongs to the specified interval, ordered by key value
         /// 
         /// </returns>
-#if USE_GENERICS
         V[] Get(Key from, Key till);
-#else
-        IPersistent[] Get(Key from, Key till);
-#endif
 
         /// <summary> Get objects which key value belongs to the specified inclusive range.
         /// Either from boundary, either till boundary either both of them can be <code>null</code>.
@@ -113,22 +87,14 @@ namespace NachoDB
         /// <returns>array of objects which keys belongs to the specified interval, ordered by key value
         /// 
         /// </returns>
-#if USE_GENERICS
         V[] Get(K from, K till);
-#else
-        IPersistent[] Get(object from, object till);
-#endif
 
         /// <summary> Get objects which key starts with specifid prefix.
         /// </summary>
         /// <param name="prefix">String key prefix</param>
         /// <returns>array of objects which key starts with specifid prefix, ordered by key value 
         /// </returns>
-#if USE_GENERICS
         V[] GetPrefix(string prefix);
-#else
-        IPersistent[] GetPrefix(string prefix);
-#endif
 
         /// <summary> 
         /// Locate all objects which key is prefix of specified word.
@@ -136,11 +102,7 @@ namespace NachoDB
         /// <param name="word">string which prefixes are located in index</param>
         /// <returns>array of objects which key is prefix of specified word, ordered by key value
         /// </returns>
-#if USE_GENERICS
         V[] PrefixSearch(string word);
-#else
-        IPersistent[] PrefixSearch(string word);
-#endif
 
         /// <summary> Get number of objects in the index
         /// </summary>
@@ -148,21 +110,11 @@ namespace NachoDB
         /// </returns>
         int Size();
 
-#if !USE_GENERICS
-        /// <summary> Remove all objects from the index
-        /// </summary>
-        void  Clear();
-#endif
-
         /// <summary> Get all objects in the index as array orderd by index key
         /// </summary>
         /// <returns>array of objects in the index ordered by key value
         /// </returns>
-#if USE_GENERICS
         V[] ToArray();
-#else
-        IPersistent[] ToArray();
-#endif
 
         /// <summary> Get all objects in the index as array of specified type orderd by index key
         /// </summary>
@@ -182,11 +134,7 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>selection iterator</returns>
         ///
-#if USE_GENERICS
         IEnumerator<V> GetEnumerator(Key from, Key till, IterationOrder order);
-#else
-        IEnumerator GetEnumerator(Key from, Key till, IterationOrder order);
-#endif
 
         /// <summary>
         /// Get iterator for traversing objects in the index with key belonging to the specified range. 
@@ -199,11 +147,7 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>selection iterator</returns>
         ///
-#if USE_GENERICS
         IEnumerator<V> GetEnumerator(K from, K till, IterationOrder order);
-#else
-        IEnumerator GetEnumerator(object from, object till, IterationOrder order);
-#endif
 
         /// <summary>
         /// Get iterator for traversing objects in ascent order belonging to the specified range. 
@@ -216,11 +160,7 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>selection iterator</returns>
         ///
-#if USE_GENERICS
         IEnumerator<V> GetEnumerator(Key from, Key till);
-#else
-        IEnumerator GetEnumerator(Key from, Key till);
-#endif
 
         /// <summary>
         /// Get iterator for traversing objects in ascent order belonging to the specified range. 
@@ -233,11 +173,7 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>selection iterator</returns>
         ///
-#if USE_GENERICS
         IEnumerator<V> GetEnumerator(K from, K till);
-#else
-        IEnumerator GetEnumerator(object from, object till);
-#endif
 
         /// <summary>
         /// Get iterator for traversing objects in ascent order which key starts with specified prefix. 
@@ -246,11 +182,7 @@ namespace NachoDB
         /// <param name="prefix">String key prefix</param>
         /// <returns>selection iterator</returns>
         ///
-#if USE_GENERICS
         IEnumerator<V> GetEnumerator(string prefix);
-#else
-        IEnumerator GetEnumerator(string prefix);
-#endif
 
         /// <summary>
         /// Get enumerable collection of objects in the index with key belonging to the specified range. 
@@ -263,11 +195,7 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> Range(Key from, Key till, IterationOrder order);
-#else
-        IEnumerable Range(Key from, Key till, IterationOrder order);
-#endif
 
         /// <summary>
         /// Get enumerable ascent ordered collection of objects in the index with key belonging to the specified range. 
@@ -279,11 +207,7 @@ namespace NachoDB
         /// High boundary can be inclusive or exclusive.</param>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> Range(Key from, Key till);
-#else
-        IEnumerable Range(Key from, Key till);
-#endif
 
         /// <summary>
         /// Get enumerable collection of objects in the index with key belonging to the specified range. 
@@ -294,22 +218,14 @@ namespace NachoDB
         /// <param name="order"><code>IterationOrder.AscentOrder</code> or <code>IterationOrder.DescentOrder</code></param>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> Range(K from, K till, IterationOrder order);
-#else
-        IEnumerable Range(object from, object till, IterationOrder order);
-#endif
 
         /// <summary>
         /// Get enumerable collection of objects in descending order
         /// </summary>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> Reverse();
-#else
-        IEnumerable Reverse();
-#endif
         
         /// <summary>
         /// Get enumerable ascent ordered collection of objects in the index with key belonging to the specified range. 
@@ -319,11 +235,7 @@ namespace NachoDB
         /// <param name="till">Inclusive high boundary. If <code>null</code> then high boundary is not specified.</param>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> Range(K from, K till);
-#else
-        IEnumerable Range(object from, object till);
-#endif
 
         /// <summary>
         /// Get enumerable ascent ordered collection of objects in the index which key starts with specified prefix. 
@@ -332,11 +244,7 @@ namespace NachoDB
         /// <param name="prefix">String key prefix</param>
         /// <returns>enumerable collection</returns>
         ///
-#if USE_GENERICS
         IEnumerable<V> StartsWith(string prefix);
-#else
-        IEnumerable StartsWith(string prefix);
-#endif
 
         /// <summary>
         /// Get iterator for traversing all entries in the index 
