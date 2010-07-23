8c97a9f4b444283bb638f5ddd9feb59c5f846d4c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-07-22 18:35:35 -0700

    improve xml test

--- csharp/tests/TestXML/TestXML.cs
+++ csharp/tests/TestXML/TestXML.cs
@@ -1,107 +1,11 @@
 using System;
+using System;
 using NachoDB;
-using System.Diagnostics;
 
-public class TestXML
+public class TestXMLRunner
 {
-    class Record : Persistent
-    {
-        internal String strKey;
-        internal long   intKey;
-        internal double realKey;
-    }
-
-    struct Point {
-        public int x;
-        public int y;
-    }
-
-    class Root : Persistent
-    {
-        internal Index<string,Record>    strIndex;
-        internal FieldIndex<long,Record> intIndex;
-        internal MultiFieldIndex<Record> compoundIndex;
-        internal Point      point;
-    }
-
-    internal const int nRecords = 100000;
-    internal static int pagePoolSize = 32 * 1024 * 1024;
-
     static public void  Main(System.String[] args)
     {
-        int i;
-        Storage db = StorageFactory.CreateStorage();
-
-        db.Open("test1.dbs", pagePoolSize);
-        Root root = (Root) db.Root;
-        if (root == null)
-        {
-            root = new Root();
-            root.strIndex = db.CreateIndex<string,Record>(true);
-            root.intIndex = db.CreateFieldIndex<long,Record>("intKey", true);
-            root.compoundIndex = db.CreateFieldIndex<Record>(new string[]{"strKey", "intKey"}, true);
-            root.point.x = 1;
-            root.point.y = 2;
-            db.Root = root;
-        }
-        FieldIndex<long,Record> intIndex = root.intIndex;
-        MultiFieldIndex<Record> compoundIndex = root.compoundIndex;
-        Index<string,Record> strIndex = root.strIndex;
-        
-        DateTime start = DateTime.Now;
-        long key = 1999;
-        for (i = 0; i < nRecords; i++)
-        {
-            Record rec = new Record();
-            key = (3141592621L * key + 2718281829L) % 1000000007L;
-            rec.intKey = key;
-            rec.strKey = System.Convert.ToString(key);
-            rec.realKey = (double)key;
-            intIndex.Put(rec);
-            strIndex.Put(new Key(rec.strKey), rec);
-            compoundIndex.Put(rec);                
-        }
-        db.Commit();
-        System.Console.WriteLine("Elapsed time for inserting " + nRecords + " records: " + (DateTime.Now - start));
-
-        start = DateTime.Now;
-        System.IO.StreamWriter writer = new System.IO.StreamWriter("test.xml");
-        db.ExportXML(writer);
-        writer.Close();
-        System.Console.WriteLine("Elapsed time for XML export: " + (DateTime.Now - start));
-        
-        db.Close();
-        db.Open("test2.dbs", pagePoolSize);
-
-        start = DateTime.Now;
-        System.IO.StreamReader reader = new System.IO.StreamReader("test.xml");
-        db.ImportXML(reader);
-        reader.Close();
-        System.Console.WriteLine("Elapsed time for XML import: " + (DateTime.Now - start));
-
-        root = (Root)db.Root;
-        intIndex = root.intIndex;
-        strIndex = root.strIndex;
-        compoundIndex = root.compoundIndex;
-        Debug.Assert(root.point.x == 1 && root.point.y == 2);
-
-        start = DateTime.Now;
-        key = 1999;
-        for (i = 0; i < nRecords; i++)
-        {
-            key = (3141592621L * key + 2718281829L) % 1000000007L;
-            String strKey = System.Convert.ToString(key);
-            Record rec1 = intIndex[key];
-            Record rec2 = strIndex[strKey];
-            Record rec3 = compoundIndex.Get(new Key(strKey, key));
-            Debug.Assert(rec1 != null);
-            Debug.Assert(rec1 == rec2);
-            Debug.Assert(rec1 == rec3);
-            Debug.Assert(rec1.intKey == key);
-            Debug.Assert(rec1.realKey == (double)key);
-            Debug.Assert(strKey.Equals(rec1.strKey));
-        }
-        System.Console.WriteLine("Elapsed time for performing " + nRecords * 2 + " index searches: " + (DateTime.Now - start));
-        db.Close();
+        TestXml.Run(100000, false);
     }
 }
--- csharp/tests/Tests.cs
+++ csharp/tests/Tests.cs
@@ -202,32 +202,50 @@ namespace NachoDB
             internal double realKey;
         }
 
+        struct Point
+        {
+            public int x;
+            public int y;
+        }
+
         class Root : Persistent
         {
             internal Index<string, Record> strIndex;
             internal FieldIndex<long, Record> intIndex;
+            internal MultiFieldIndex<Record> compoundIndex;
+            internal Point      point;
         }
 
         internal static int pagePoolSize = 32 * 1024 * 1024;
 
         public static void Run(int nRecords, bool useAltBtree)
         {
-            string dbName = @"testxml.dbs";
+            string dbName1 = @"testxml1.dbs";
+            string dbName2 = @"testxml2.dbs";
+            UnitTests.SafeDeleteFile(dbName1);
+            UnitTests.SafeDeleteFile(dbName2);
+
             string xmlName = useAltBtree ? @"testalt.xml" : @"test.xml";
-            UnitTests.SafeDeleteFile(dbName);
             Storage db = StorageFactory.CreateStorage();
             db.AlternativeBtree = useAltBtree;
-            db.Open(dbName, pagePoolSize);
+            db.Open(dbName1, pagePoolSize);
+
             Root root = (Root)db.Root;
             UnitTests.AssertThat(null == root);
             root = new Root();
             root.strIndex = db.CreateIndex<string, Record>(true);
             root.intIndex = db.CreateFieldIndex<long, Record>("intKey", true);
+            root.compoundIndex = db.CreateFieldIndex<Record>(new string[]{"strKey", "intKey"}, true);
+            root.point.x = 1;
+            root.point.y = 2;
             db.Root = root;
-            DateTime start = DateTime.Now;
-            long key = 1999;
+
+            //DateTime start = DateTime.Now;
             Index<string, Record> strIndex = root.strIndex;
             FieldIndex<long, Record> intIndex = root.intIndex;
+            MultiFieldIndex<Record> compoundIndex = root.compoundIndex;
+
+            long key = 1999;
             int i;
             for (i = 0; i < nRecords; i++)
             {
@@ -238,24 +256,32 @@ namespace NachoDB
                 rec.realKey = (double)key;
                 strIndex.Put(new Key(rec.strKey), rec);
                 intIndex.Put(rec);
+                compoundIndex.Put(rec);                
             }
             db.Commit();
+            //System.Console.WriteLine("Elapsed time for inserting " + nRecords + " records: " + (DateTime.Now - start));
 
+            // start = DateTime.Now
             System.IO.StreamWriter writer = new System.IO.StreamWriter(xmlName);
             db.ExportXML(writer);
             writer.Close();
             db.Close();
+            //System.Console.WriteLine("Elapsed time for XML export: " + (DateTime.Now - start));
 
-            UnitTests.SafeDeleteFile(dbName);
-            db.Open(dbName, pagePoolSize);
+            //start = DateTime.Now;
+            db.Open(dbName2, pagePoolSize);
             System.IO.StreamReader reader = new System.IO.StreamReader(xmlName);
             db.ImportXML(reader);
             reader.Close();
+            //System.Console.WriteLine("Elapsed time for XML import: " + (DateTime.Now - start));
 
             root = (Root)db.Root;
             strIndex = root.strIndex;
             intIndex = root.intIndex;
+            compoundIndex = root.compoundIndex;
+            UnitTests.AssertThat(root.point.x == 1 && root.point.y == 2);
 
+            //start = DateTime.Now;
             key = 1999;
             for (i = 0; i < nRecords; i++)
             {
@@ -263,17 +289,16 @@ namespace NachoDB
                 String strKey = System.Convert.ToString(key);
                 Record rec1 = strIndex[strKey];
                 Record rec2 = intIndex[key];
-                //Record rec3 = compoundIndex.Get(new Key(strKey, key));
+                Record rec3 = compoundIndex.Get(new Key(strKey, key));
                 UnitTests.AssertThat(rec1 != null);
                 UnitTests.AssertThat(rec1 == rec2);
+                UnitTests.AssertThat(rec1 == rec3);
                 UnitTests.AssertThat(rec1.intKey == key);
                 UnitTests.AssertThat(rec1.realKey == (double)key);
                 UnitTests.AssertThat(strKey.Equals(rec1.strKey));
-                /*
-                Debug.Assert(rec1 == rec3);
-                */
             }
             db.Close();
+            //System.Console.WriteLine("Elapsed time for performing " + nRecords * 2 + " index searches: " + (DateTime.Now - start));
         }
     }
 
