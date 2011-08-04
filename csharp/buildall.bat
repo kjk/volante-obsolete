174cdcb8c80a7cfdf38e6f4ea85a2db94c0edda6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-08-03 17:29:36 -0700

    build more configuration from buildall.bat

--- csharp/buildall.bat
+++ csharp/buildall.bat
@@ -7,13 +7,19 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.ba
 IF ERRORLEVEL 1 GOTO NEEDSVS
 
 :BUILD
-nmake -f makefile.msvc
+nmake -f makefile.msvc CFG=dbg
 IF ERRORLEVEL 1 GOTO COMPILE_FAILED
 
-nmake -f makefile.msvc OMIT_XML=yes
+nmake -f makefile.msvc CFG=rel OMIT_XML=yes
 IF ERRORLEVEL 1 GOTO COMPILE_FAILED
 
-nmake -f makefile.msvc OMIT_REPLICATION=yes
+nmake -f makefile.msvc CFG=rel OMIT_REPLICATION=yes
+IF ERRORLEVEL 1 GOTO COMPILE_FAILED
+
+nmake -f makefile.msvc CFG=rel OMIT_RAW_TYPE=yes
+IF ERRORLEVEL 1 GOTO COMPILE_FAILED
+
+nmake -f makefile.msvc CFG=rel OMIT_ALL=yes
 IF ERRORLEVEL 1 GOTO COMPILE_FAILED
 
 nmake -f makefile.msvc CFG=rel
