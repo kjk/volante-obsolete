447c8f6545e33094594faa7eb6fef3cef3535208
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-08-06 03:46:50 -0700

    don't pollute top directory with databases when running tests

--- csharp/runtests.bat
+++ csharp/runtests.bat
@@ -1,21 +1,22 @@
 call "%ProgramFiles%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
-IF ERRORLEVEL 1 GOTO TRYX86
-GOTO BUILD
+@IF ERRORLEVEL 1 GOTO TRYX86
+@GOTO BUILD
 
 :TRYX86
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
-IF ERRORLEVEL 1 GOTO NEEDSVS
+@IF ERRORLEVEL 1 GOTO NEEDSVS
 
 :BUILD
-devenv Volante.sln /Project tests\Tests\Tests.csproj /ProjectConfig Release /Rebuild
-IF ERRORLEVEL 1 GOTO FAILEDCOMPILE
+devenv Volante.sln /Rebuild Debug /Project Tests
+@IF ERRORLEVEL 1 GOTO FAILEDCOMPILE
 
-bin\Release\Tests.exe -fast
+@cd bin\Debug
+Tests.exe -fast
 
 @rem start %O%\TestReplic master
 @rem %O%\TestReplic slave
 
-goto END
+@goto END
 
 :FAILEDCOMPILE
 echo "Compilcation failed"
